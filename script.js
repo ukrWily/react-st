55456cfdde476dfd7ca2fa318d669380a51ade15
”ukrWily” <ukrwily@gmail.com>
2024-07-29 19:59:30 +0300

    PC first

---
+++ script.js
@@ -0,0 +1,18 @@
+const App = () => {
+  const [buttonText, setButtonText] = React.useState("Click me");
+
+  const onButtonClick = () => {
+    setButtonText(`Hello from React ${Math.random()}`);
+  };
+
+  return (
+    <div className="app">
+      <button onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  );
+};
+
+const head = <h1>Hello</h1>;
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
